cc6e859d7448d461167ed26043bd63701e97b2c0
ianic-dev <ian@ianic.dev>
2025-01-08 15:33:52 +0100

    closing panel attachments done (for now)

--- sandwich_panel.py
+++ sandwich_panel.py
@@ -8,12 +8,13 @@ fabric_thickness=0
 class Sandwich_Panel:
     def __init__(self,components: list[list]):
         inner_radius = 0.58
-        a = b = 1.679 # 2*(inner_radius+0.4) 1.679
+        a = 1.679 # 2*(inner_radius+0.4) 1.679
+        b = 1.44
         self.components = components
         self.density=self.panel_density()
         self.width=a
         self.length=b
-        self.area = a * b - np.pi * inner_radius**2
+        self.area = a * b # - np.pi * inner_radius**2
 
         self.panel_mass = self.density * self.area
 
@@ -38,21 +39,18 @@ class Sandwich_Panel:
 
 
 def sandwich_panel_get():
-    panel_upper_closing: list[list] = [gc('quarter solar panel'), gc('quarter radiator'), gc('quarter louver'), gc('rcs thruster', 3)]
-    panel_upper_inner: list[list] = [gc('quarter solar panel'), gc('quarter radiator'), gc('quarter louver'), gc('HRSC camera'), gc('UV spectrometer'), gc('a-BELA'), gc('gyroscope', 2), gc('rcs thruster', 3)]
-    panel_lower: list[list] = [gc('quarter solar panel'), gc('quarter radiator'), gc('quarter louver'), gc('HRSC electronics'), gc('UV electronics'), gc('reaction wheel', 4), gc('magnetometer electronics'), gc('battery'), gc('PCDS'), gc('cables'), gc('rcs thruster', 3)]
-    panel_lower_closing: list[list] = [gc('quarter solar panel'), gc('quarter radiator'), gc('quarter louver'), gc('rcs thruster', 3)]
+    panel_solar: list[list] = [gc('quarter solar panel', 2), gc('rcs thruster', 3)]
+    panel_radiator: list[list] = [gc('quarter radiator', 4), gc('quarter louver', 4), gc('rcs thruster', 3)]
 
-    panel_0 = Sandwich_Panel(panel_upper_closing)
-    panel_1 = Sandwich_Panel(panel_upper_inner)
-    panel_2 = Sandwich_Panel(panel_lower)
-    panel_3 = Sandwich_Panel(panel_lower_closing)
-    panels = [panel_0, panel_1, panel_2, panel_3]
+    panel_0 = Sandwich_Panel(panel_solar)
+    panel_0.total_mass += 0.2 # magnetometer
+    panel_1 = Sandwich_Panel(panel_radiator)
+    panels = [panel_0, panel_1]
 
     for panel in panels:
-        panel.total_mass += 0.375 + (3*0.106/4) + 0.1 + panel.width * 1.44 * panel.density
+        panel.total_mass += 0.375 + (3*0.106/4) + 0.1
         print(panel.total_mass)
-        # sun sensor, star sensor, magnetometer and outer closing panels
+        # sun sensor, star sensor
 
     return panels
 
